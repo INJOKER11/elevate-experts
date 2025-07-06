bfad92a7681a71d41df42adfeab6133ae24a9350
Pavel <mig20047@gmail.com>
2025-07-06 16:29:56 +0200

    Add mobile and tablet header menu

---
+++ public/js/burger_menu.js
@@ -0,0 +1,34 @@
+
+
+export function burger_menu() {
+    const openButton = document.querySelector(".header_burger_menu_open_button");
+    const closeButton = document.querySelector(".header_burger_menu_close_button");
+    const overlay = document.querySelector(".mobile_header_menu_overlay");
+    const headerMenu = document.querySelector(".mobile_header_menu");
+
+    const body = document.body;
+
+    overlay.addEventListener("click", () => {
+        headerMenu.style.display = "none";
+        overlay.style.display = "none";
+        closeButton.style.display = "none";
+        openButton.style.display = "flex";
+        body.classList.remove("no-scroll");
+    });
+
+    openButton.addEventListener('click', () => {
+        headerMenu.style.display = "flex";
+        openButton.style.display = "none";
+        closeButton.style.display = "flex";
+        overlay.style.display = "flex";
+        body.classList.add("no-scroll");
+    })
+
+    closeButton.addEventListener("click", () => {
+        headerMenu.style.display = "none";
+        overlay.style.display = "none";
+        closeButton.style.display = "none";
+        openButton.style.display = "flex";
+        body.classList.remove("no-scroll");
+    })
+}
---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+import {burger_menu} from "./burger_menu.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    burger_menu();
+})
