8729826f1573cad135d1b2434199285bad62907f
Pavel <mig20047@gmail.com>
2025-07-10 21:19:30 +0200

    Save

--- public/js/formModal.js
+++ public/js/formModal.js
@@ -1,5 +1,24 @@
 
 
-export const formModal() {
+export function formModal() {
+    const modalOverlay = document.querySelector(".form_modal_overlay");
+    const body = document.body;
+    const modalCloseButton = document.querySelector(".form_modal_close_button");
+    const openButton = document.querySelector("#header_button")
 
+    openButton.addEventListener("click", () => {
+        modalOverlay.classList.add("open");
+        body.classList.add("no-scroll")
+    })
+    modalOverlay.addEventListener('click', (ev) => {
+        if (ev.target === modalOverlay) {
+            modalOverlay.classList.remove("open");
+            body.classList.remove("no-scroll")
+        }
+    });
+
+    modalCloseButton.addEventListener('click', () => {
+        modalOverlay.classList.remove("open");
+        body.classList.remove("no-scroll")
+    });
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,11 @@
 import {burgerMenu} from "./burgerMenu.js";
 import {questions} from "./questions.js";
 import {successModal} from "./successModal.js";
+import {formModal} from "./formModal.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     burgerMenu();
     questions();
     successModal();
+    formModal();
 })
