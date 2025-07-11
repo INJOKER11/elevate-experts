6c20b1f4da59b0abc3d9197196dabfe25fb5964c
Pavel <mig20047@gmail.com>
2025-07-11 22:18:39 +0200

    Add carousel

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,12 @@ import {burgerMenu} from "./burgerMenu.js";
 import {questions} from "./questions.js";
 import {successModal} from "./successModal.js";
 import {formModal} from "./formModal.js";
+import {carousel} from "./carousel.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     burgerMenu();
     questions();
     successModal();
     formModal();
+    carousel();
 })
